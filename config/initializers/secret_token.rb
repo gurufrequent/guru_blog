9088bbed1ee4d1227133429828815253f5967302
unknown <gprasa2@.gar.corp.intel.com>
2010-12-07 14:40:55 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuruBlog::Application.config.secret_token = '0819d28124fe8b288c9ae4ff50d7f2cda5d616343bb566536e8629f6cbc979af95b37dce71214e5732c3f77ec5f17bd8c797d99d33e90c7e33a0d3bf044e8083'
